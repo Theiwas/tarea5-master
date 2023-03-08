6f330d1f85bd60f1807c69c9af4ad2de90e52074
Theiwas <jonathan.santiago.cardenas@hotmail.com>
2023-03-07 19:57:21 -0600

    first commit

---
+++ tarea5-master/src/components/CardComp.vue
@@ -0,0 +1,39 @@
+<template>
+	<div class="card">
+		<div class="card-body">
+			<h5 v-if="title" class="card-title">{{ title }}</h5>
+			<p v-if="parrafo" class="card-text mb-1">
+				{{ parrafo }}
+			</p>
+			<img v-if="picture" :src="picture" alt="post img" class="post-img" />
+			<slot></slot>
+			<div class="d-grid gap-2">
+				<button
+					v-for="btn in btns"
+					:key="btn.txt"
+					class="btn"
+					:class="btn.class">
+					{{ btn.txt }}
+				</button>
+				<slot name="footer"></slot>
+			</div>
+		</div>
+	</div>
+</template>
+
+<script>
+	export default {
+		name: 'CardComp',
+		props: ['title', 'parrafo', 'btns', 'picture'],
+	}
+</script>
+
+<style scoped>
+	.card {
+		border: none;
+		box-shadow: 0 2px 4px rgb(0 0 0 / 10%), 0 8px 16px rgb(0 0 0 / 10%);
+	}
+	.post-img {
+		max-width: 100%;
+	}
+</style>
